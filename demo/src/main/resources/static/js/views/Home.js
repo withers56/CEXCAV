5ef6bcc346c40d6a3db14d6ac7823fbabbfe307a
William <william.k.withers@gmail.com>
2024-04-01 11:11:29 -0500

    added frontend form

--- demo/src/main/resources/static/js/views/Home.js
+++ demo/src/main/resources/static/js/views/Home.js
@@ -2,7 +2,7 @@ export default function Home(props) {
     console.log("The frontend did it. HER FAULT");
     return `
     <nav class="d-flex justify-content-between">
-        <span class="logo">Coyote Excavation</span>
+        <span class="logo"><b>Coyote Excavation</b></span>
         <div class="align-self-center me-3 me-md-5">
             <a href="https://ebq.com/contact-us/" target="_blank"><button class="btn-blue btn-text contact-btn"><b>CONTACT US</b></button></a>
         </div>
@@ -10,15 +10,18 @@ export default function Home(props) {
     <div class="container-fluid">
         <div class="row row-margin first-section padding-y">
             <div class="col-12 col-md-6 section">
-                <div class="title-header">Integrated <b><br>Marketing Service</b></div>
-                <p class="mt-3">We turn your marketing plans to action by building on your ideas and executing initiatives alongside you and your team. Get a complete department that can pivot to different ideas of marketing based on your needs.</p>
+                <div class="title-header">About <b><br>Me Section</b></div>
+                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit debitis maxime praesentium, est pariatur ea, dolorem laborum voluptas nihil modi adipisci dolore necessitatibus minima. Maxime vero praesentium ex sunt. Quisquam, modi. Obcaecati aut aliquid sunt ad fugit ullam, odit illum vitae itaque, rem, neque magnam? Commodi ab id facilis voluptatibus delectus aspernatur, cum iusto amet est in nemo rem asperiores.
+                </p>
             </div>
         </div>
         <div class="row white-bg row-margin padding-y">
             <img src="../images/test.jpg" alt="" class="col-12 col-md-6 my-5 order-md-2 ps-md-5">
             <div class="col-12 col-md-6 section order-md-1 pe-md-5">
-                <p class="header">We're experts with <b>modern marketing tools,</b> so you don't have to be!</p>
-                <p>With hands-on experience in B2B's most innovative apps and tools, we take a data-driven approach to continually improve prospect engagement. Whether we work directly out of your platform ori provide out own insights manually, you always get full visibility into the process</p>
+                <p class="header">Service number 1 <b>That we are providing,</b> so you don't have to!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, consequatur numquam aspernatur ipsum quos corporis saepe quo quae temporibus alias quaerat. Facere, vitae fugit asperiores quae libero iure. Consectetur recusandae odio commodi aperiam unde accusantium placeat vel maxime laudantium delectus! Facilis suscipit fuga id nobis? Natus non nesciunt magnam quia.</p>
+                <p class="header mt-3">Service number 2 <b>That we are providing,</b> so you don't have to!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, consequatur numquam aspernatur ipsum quos corporis saepe quo quae temporibus alias quaerat. Facere, vitae fugit asperiores quae libero iure. Consectetur recusandae odio commodi aperiam unde accusantium placeat vel maxime laudantium delectus! Facilis suscipit fuga id nobis? Natus non nesciunt magnam quia.</p>
             </div>
         </div>
         <div class="row white-bg row-margin mb-5">
@@ -30,6 +33,38 @@ export default function Home(props) {
             <img src="../images/IMG_1229.jpg" alt="" class="col-12 col-lg-6 my-5 order-lg-1 pe-md-5" id="dept-image">
         </div>
     </div>
+    <div class="row white-bg row-margin padding-y">
+            <div class="col-6">Contact Us</div>
+            <div class="col-6">
+                <div class="row">
+                    <div class="col mb-3">
+                        <label for="firstNameInput" class="form-label">First Name</label>  
+                        <input type="text" class="form-control" aria-label="First name" id="firstNameInput">
+                    </div>
+                    <div class="col mb-3">
+                        <label for="lastNameInput" class="form-label">Last Name</label>  
+                        <input type="text" class="form-control" aria-label="Last name" id="lastNameInput">
+                    </div>
+                    <div class="mb-3">
+                        <label for="emailInput" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="emailInput">
+                    </div>
+                    <div class="mb-3">
+                        <label for="phoneNumberInput" class="form-label">Phone Number</label>
+                        <input type="tel" class="form-control" id="phoneNumberInput" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="123-456-7890"required>
+                        <br>
+                        <small>Format: 123-456-7890</small>
+                    </div>
+                    <div class="mb-3">
+                        <label for="messageInput" class="form-label">Message</label>
+                        <textarea class="form-control" id="messageInput" rows="3"></textarea>
+                    </div>
+                    <div class="col-12">
+                        <button class="btn btn-primary" type="submit" id="contactFormBtn">Submit form</button>
+                    </div>
+                </div>
+            </div>
+    </div>
     <footer class="footer">
         <p class="header">Looking to take your <b>marketing to the next level?</b></p>
         <a href="https://ebq.com/contact-us/" target="_blank"><button class="btn-orange btn-text contact-btn mt-5"><b>CONTACT US</b></button></a>
@@ -38,5 +73,22 @@ export default function Home(props) {
 }
 
 export function HomeEvents() {
-    
+    submitFormBtn();
+}
+
+
+function submitFormBtn(){
+    document.querySelector('#contactFormBtn').addEventListener("click", function(){
+
+
+        let name = document.querySelector('#firstNameInput').value + " " + document.querySelector('#lastNameInput').value;
+        let email = document.querySelector('#emailInput').value;
+        let phoneNumber = document.querySelector('#phoneNumberInput').value
+        let message = document.querySelector('#messageInput').value
+        console.log(name);
+        console.log(email);
+        console.log(phoneNumber);
+        console.log(message);
+
+    })
 }
